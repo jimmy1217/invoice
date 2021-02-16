9dba429a4be262c80393cc58fbbed819d656980e
jimmy <jimmy741217@gmail.com>
2021-02-17 02:21:01 +0800

    index

--- pages/index.js
+++ pages/index.js
@@ -1,2 +1,5 @@
-const IndexPage = () => <div>invoice api</div>
+const IndexPage = () => <div>
+    <p>List API:https://invoice-nine.vercel.app/api/list</p>
+    <p>Detail API:https://invoice-nine.vercel.app/api/detail/{link_id}</p>
+</div>
 export default IndexPage

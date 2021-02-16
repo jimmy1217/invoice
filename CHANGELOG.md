aa87ef6f33a5b059c40dd916e636759219b73847
jimmy <jimmy741217@gmail.com>
2021-02-17 00:25:06 +0800

    api 開出 list/detail

---
+++ CHANGELOG.md
@@ -0,0 +1,3 @@
+#### v0.0.1
+##### init
+- api 開出 list/detail
--- app.js
+++ app.js
@@ -5,7 +5,7 @@ var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 
 var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
+var apiRouter = require('./api/apiRouter');
 
 var app = express();
 
@@ -19,8 +19,11 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
+// view routes
 app.use('/', indexRouter);
-app.use('/users', usersRouter);
+
+// api routes
+app.use('/api',apiRouter)
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {

406a3c8675efef1f2af9c1a61c189f68af64d95f
jimmy <jimmy741217@gmail.com>
2021-02-17 02:28:42 +0800

    id

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,5 @@
 const IndexPage = () => <div>
-    <p>List API:https://invoice-nine.vercel.app/api/list</p>
-    <p>Detail API:https://invoice-nine.vercel.app/api/detail/{link_id}</p>
+    <p>List API - https://invoice-nine.vercel.app/api/list</p>
+    <p>Detail API - https://invoice-nine.vercel.app/api/detail/{"{dataLink_id}"}</p>
 </div>
 export default IndexPage

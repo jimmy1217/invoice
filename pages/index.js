12fa63fb9cabdc4469ce30eb1435f7f5ad627526
jimmy <jimmy741217@gmail.com>
2021-02-18 00:20:05 +0800

    index

--- pages/index.js
+++ pages/index.js
@@ -1,21 +1,16 @@
-import React, { useEffect } from 'react';
-const cheerio = require('cheerio')
+// const cheerio = require('cheerio')
 
-const fetchData = async () => {
-    const res = await fetch('https://invoice-nine.vercel.app/all', { method: 'get' })
-    const html = await res.text();
-    const $ = cheerio.load(html)
-    const list_data = JSON.parse($('#list_source').text())
-    const detail_data = JSON.parse($('#detail_source').text())
-    console.log(list_data)
-    console.log(detail_data)
-}
+// const fetchData = async () => {
+//     const res = await fetch('https://invoice-nine.vercel.app/all', { method: 'get' })
+//     const html = await res.text();
+//     const $ = cheerio.load(html)
+//     const list_data = JSON.parse($('#list_source').text())
+//     const detail_data = JSON.parse($('#detail_source').text())
+//     console.log(list_data)
+//     console.log(detail_data)
+// }
 
 const IndexPage = () => {
-    useEffect(() => {
-        fetchData()
-        console.log('did ')
-    }, []);
     return (
         <div>
             <p>List API - https://invoice-nine.vercel.app/api/list</p>

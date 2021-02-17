b5092eacc092340ca365f08ba4226839fb0f5688
jimmy <jimmy741217@gmail.com>
2021-02-17 16:15:15 +0800

    test

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,20 @@
-const IndexPage = () => <div>
-    <p>List API - https://invoice-nine.vercel.app/api/list</p>
-    <p>Detail API - https://invoice-nine.vercel.app/api/detail/{"{dataLink_id}"}</p>
-</div>
+import React, { useEffect } from 'react';
+
+const fetchData = async () => {
+    const res = await fetch('https://invoice-nine.vercel.app/list')
+    console.log(res.data)
+}
+
+const IndexPage = () => {
+    useEffect(() => {
+        fetchData()
+        console.log('did ')
+    }, []);
+    return (
+        <div>
+            <p>List API - https://invoice-nine.vercel.app/api/list</p>
+            <p>Detail API - https://invoice-nine.vercel.app/api/detail/{"{dataLink_id}"}</p>
+        </div>
+    )
+}
 export default IndexPage

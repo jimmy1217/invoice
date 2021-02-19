c51e369bae4386529001459ad5f2bdcd4d5d63d0
jimmy <jimmy741217@gmail.com>
2021-02-20 01:39:59 +0800

    month select

--- src/components/SwipeList/index.js
+++ src/components/SwipeList/index.js
@@ -2,15 +2,44 @@ import React, { useState } from 'react';
 import AliceCarousel from 'react-alice-carousel';
 import Style from './SwipeList.module.css'
 
-const ListItem = ({ item, detail, years, index, activeIndex }) => {
+const RangeSelect = ({ item, invoiceList, index, onSlideChanged }) => {
+    const monthsData = invoiceList.data
+        .map((originItem, k) => ({
+            ...originItem,
+            year: Number(originItem.year) + 1911,
+            pageIndex: k
+        }))
+        .filter((item, i) => i >= index - 2 && i < index + 6)
+        .splice(0, 5)
+    return (
+        <div className={`${Style.roundTitle} ${Style.circleRound}`}>
+            {<div className={`${Style.rangeTitle} ${Style.circleRound}`}>
+                {item.monthRange}
+            </div>}
+            {
+                <div className={`${Style.selectRange}`} >
+                    {monthsData.map(monthItem =>
+                        <span
+                            key={`${index}_${monthItem.pageIndex}`}
+                            onClick={() => {
+                                onSlideChanged({ item: monthItem.pageIndex })
+                            }}>
+                            <p>{monthItem.year}</p>
+                            <h3>{monthItem.monthRange}</h3>
+                        </span>)}
+
+                </div>}
+        </div>
+    )
+}
+
+
+
+const ListItem = ({ invoiceList, item, detail, years, index, activeIndex, onSlideChanged }) => {
     return (
         <div key={`${years}${item.dataLink}`} className={Style.swipeMain}>
             <div className={Style.monthHeader}>
-                <div className={`${Style.roundTitle} ${Style.circleRound}`}>
-                    <div className={`${Style.rangeTitle} ${Style.circleRound}`}>
-                        {item.monthRange}
-                    </div>
-                </div>
+                <RangeSelect item={item} invoiceList={invoiceList} index={index} onSlideChanged={onSlideChanged} />
                 <h2>
                     {years}
                 </h2>
@@ -29,7 +58,8 @@ const ListItem = ({ item, detail, years, index, activeIndex }) => {
 }
 
 function ListEqual(prevProps, nextProps) {
-    return !(nextProps.activeIndex === nextProps.index)
+    return false
+    // return !(nextProps.activeIndex === nextProps.index)
 }
 
 const ListItemComponent = React.memo(ListItem, ListEqual)
@@ -37,15 +67,24 @@ const ListItemComponent = React.memo(ListItem, ListEqual)
 const SwipeList = (props) => {
     const { invoiceList, allDetail } = props.data;
     const [activeIndex, setActiveIndex] = useState(0);
-    const ListView = invoiceList.data.map((item, i) => {
-        const detail = allDetail[item.dataLink];
-        const years = Number(item.year) + 1911;
-        return <ListItemComponent item={item} detail={detail} years={years} index={i} activeIndex={activeIndex} />
-    })
-
     const onSlideChanged = ({ item }) => {
         setActiveIndex(item)
     };
+
+    const ListView = invoiceList.data.map((item, i) => {
+        const detail = allDetail[item.dataLink];
+        const years = Number(item.year) + 1911;
+        return (
+            <ListItemComponent
+                invoiceList={invoiceList}
+                item={item}
+                detail={detail}
+                years={years}
+                index={i}
+                activeIndex={activeIndex}
+                onSlideChanged={onSlideChanged} />
+        )
+    })
 
     return (
         <AliceCarousel

c94483158e2726c0522b0c9463dc48f524bdb680
jimmy <jimmy741217@gmail.com>
2021-02-21 02:41:18 +0800

    test

--- src/components/SwipeList/SwipePage/SwipePage.js
+++ src/components/SwipeList/SwipePage/SwipePage.js
@@ -2,7 +2,7 @@ import React, { useContext, useMemo } from 'react';
 import { MyContext } from '@/store/context-manager';
 import RangeSelect from './RangeSelect';
 import Style from '@/components/SwipeList/SwipeList.module.css'
-import { GiFlatPlatform } from "react-icons/gi";
+import { GiSwirlString } from "react-icons/gi";
 
 const SwipePage = ({ invoiceList, item, detail, years, index, onSlideChanged }) => {
     const { state, dispatch } = useContext(MyContext)
@@ -20,17 +20,22 @@ const SwipePage = ({ invoiceList, item, detail, years, index, onSlideChanged }) 
                     <h2>
                         {years}
                     </h2>
+                    <div className={Style.iconBG}><GiSwirlString /></div>
                 </div>
                 <div className={`${Style.swipeContent}`}>
                     <div className={Style.detailContent}>
                         {detail.data.map((detailItem, k) => {
                             const shortCode = String(detailItem.code).substr(-3, String(detailItem.code).length);
-                            console.log(shortCode)
                             return (
-                                <p key={`${item.dataLink}_${k}`} className={Style[detailItem.type]}>
-                                    {/* <span>{detailItem.typeText}</span> */}
-                                    &zwj;{shortCode}
-                                </p>
+                                <div key={`${item.dataLink}_${k}`} className={Style[detailItem.type]}>
+
+                                    <div>
+                                        <div>{detailItem.typeText}</div>
+                                        <div>&zwj;{detailItem.code}</div>
+                                    </div>
+                                    
+                                    {/* &zwj;{shortCode} */}
+                                </div>
                             )
                         })}
                       
@@ -39,7 +44,6 @@ const SwipePage = ({ invoiceList, item, detail, years, index, onSlideChanged }) 
                         </div>
                     </div>
                 </div>
-                <div className={Style.iconBG}><GiFlatPlatform /></div>
                 {isVisible &&
                     <div className={Style.overlay}
                         onClick={() => {

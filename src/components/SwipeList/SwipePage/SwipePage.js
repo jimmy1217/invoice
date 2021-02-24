1c401ed366f3aabfd7ee84db3e00fe248c2ca7d1
jimmy <jimmy741217@gmail.com>
2021-02-25 01:21:51 +0800

    css

--- src/components/SwipeList/SwipePage/SwipePage.js
+++ src/components/SwipeList/SwipePage/SwipePage.js
@@ -78,25 +78,47 @@ const SwipePage = ({ invoiceList, item, detail, years, index, onSlideChanged }) 
                         }}>
                     </div>}
 
-                
-                    <div className={`${Style.selectRange} ${isVisible?Style.active:''}`} >
-                        {monthsData.map(monthItem =>
-                            <div
-                                key={`${index}_${monthItem.pageIndex}`}
-                                className={`${Style.rangeTitle}`}
-                                onClick={(e) => {
-                                    e.stopPropagation()
-                                    dispatch({ type: 'overlay_close' })
-                                    setTimeout(() => {
-                                        onSlideChanged({ item: monthItem.pageIndex })
-                                    }, 200)
-                                }}>
-                                <p>{monthItem.monthRange}月 {monthItem.year}</p>
-                            </div>)}
-                    </div>
+                <MonthRange
+                    isVisible={isVisible}
+                    index={index}
+                    monthsData={monthsData}
+                    onSlideChanged={onSlideChanged}
+                />
             </div>
         )
     }, [isVisible, activeIndex === index])
+}
+
+const MonthRange = ({
+    isVisible,
+    index,
+    monthsData,
+    onSlideChanged,
+}) => {
+    const { dispatch } = useContext(MyContext)
+    return useMemo(() => {
+        return (
+            <div className={`${Style.selectRange} ${isVisible ? Style.active : ''}`} >
+                {monthsData.map(monthItem => {
+                    return (
+                        <div
+                            key={`${index}_${monthItem.pageIndex}`}
+                            className={`${Style.rangeTitle}`}
+                            onClick={(e) => {
+                                e.stopPropagation()
+                                dispatch({ type: 'overlay_close' })
+                                setTimeout(() => {
+                                    onSlideChanged({ item: monthItem.pageIndex })
+                                }, 200)
+                            }}>
+                            <p>{monthItem.monthRange}月 {monthItem.year}</p>
+                        </div>
+                    )
+                }
+                )}
+            </div>
+        )
+    }, [isVisible])
 }
 
 export default SwipePage

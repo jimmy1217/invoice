5df6d308aa6c4af60f80b98aca96d22f0d8a52cc
Jimmy <jimmy20180305@gmail.com>
2022-11-23 17:34:37 +0800

    api 更新

--- pages/api/detail/[id].js
+++ pages/api/detail/[id].js
@@ -1,57 +1,49 @@
-const cheerio = require('cheerio')
-import Cors from 'cors'
-import corsMiddleware from '../../../lib/corsMiddleware'
-import AWARD from '@/constants/AWARD'
+const cheerio = require("cheerio");
+import Cors from "cors";
+import corsMiddleware from "../../../lib/corsMiddleware";
+import AWARD from "@/constants/AWARD";
 
 const cors = corsMiddleware(
-    Cors({
-        methods: ['GET'],
-    })
-)
+  Cors({
+    methods: ["GET"],
+  })
+);
 
 export default async (req, res) => {
-    await cors(req, res)
-    if (req.method === 'GET') {
-        try {
-            const { id } = req.query
-            const url = `https://www.etax.nat.gov.tw/etw-main/web/${id}`
-            const response = await fetch(url)
-            const html = await response.text();
-            const data = [];
-            const $ = cheerio.load(html)
+  await cors(req, res);
+  if (req.method === "GET") {
+    try {
+      const { id } = req.query;
+      const url = `https://invoice.etax.nat.gov.tw/${id}`;
+      const response = await fetch(url);
+      const html = await response.text();
+      const data = [];
+      const $ = cheerio.load(html);
 
-            $('.number').each((i, elem) => {
-                const typeText = $(elem).prev().text();
-                const type = AWARD.get(typeText) || 'unknown'
+      $(".etw-web .etw-color-red").each((e, elem) => {
+        const typeText = $(elem).parents("td").prev().text();
+        const type = AWARD.get(typeText) || "unknown";
+        const redValue = $(elem).text();
+        const preValue = $(elem)?.prev()?.text() ?? "";
+        const value = String(preValue + redValue).trim();
+        data.push({
+          code: value,
+          typeText,
+          type,
+        });
+      });
 
-                let value = [];
-                if ($(elem).text().trim().includes(' ')) {
-                    value = $(elem).text().trim().split(' ')
-                    value.map(item => {
-                        data.push({ code: item, typeText, type });
-                    })
-                } else if ($(elem).text().trim().includes('、')) {
-                    value = $(elem).text().trim().split('、')
-                    value.map(item => {
-                        data.push({ code: item, typeText, type });
-                    })
-                } else {
-                    value = $(elem).text().trim()
-                    data.push({ code: value, typeText, type });
-                }
-            })
+      const desc = $(".etw-color-redbrown").text();
 
-            const source = $('.des p').eq(0).text()?.trim() || $('.des p').eq(1).text()?.trim() || $('.des div').eq(0).text()?.trim();
-            const desc = source.split('，')[0]?.replace(/[\s]|、|1\.|１/g, '') ?? '';
-            res.json({
-                data,
-                desc
-            })
-        } catch (error) {
-            return res.json({
-                data: [],
-                error: `res error`
-            })
-        }
+      res.json({
+        data,
+        desc,
+      });
+    } catch (error) {
+      return res.json({
+        data: [],
+        error: `res error`,
+      });
     }
-}
+  }
+};
--- pages/api/list.js
+++ pages/api/list.js
@@ -1,44 +1,46 @@
-const cheerio = require('cheerio')
-import Cors from 'cors'
-import corsMiddleware from '../../lib/corsMiddleware'
+const cheerio = require("cheerio");
+import Cors from "cors";
+import corsMiddleware from "../../lib/corsMiddleware";
 
 const cors = corsMiddleware(
-    Cors({
-        methods: ['GET'],
-    })
-)
+  Cors({
+    methods: ["GET"],
+  })
+);
 
-const listUrl = 'https://www.etax.nat.gov.tw/etw-main/web/ETW183W1/'
+const listUrl = "https://invoice.etax.nat.gov.tw/index.html";
 
 export default async (req, res) => {
-    await cors(req, res)
-    if (req.method === 'GET') {
-        try {
-            const response = await fetch(listUrl)
-            const html = await response.text();
-            const data = [];
-            const $ = cheerio.load(html)
-            $('#fbonly tbody tr td a').filter((i, elem) => $(elem).text().split('、').length > 1).each((i, elem) => {
-                const titleString = $(elem).text();
-                const year = titleString.split('年')[0];
-                const re = /月/gi;
-                const monthRange = titleString.split('年')[1].replace('、', '~').replace(re, '');
-                const dataLink = $(elem).attr('href').split('/web/')[1]
-                data.push({
-                    year,
-                    monthRange,
-                    dataLink
-                })
-            })
-            res.statusCode = 200;
-            return res.json({
-                data
-            })
-        } catch (error) {
-            return res.json({
-                data: [],
-                error: `res error`
-            })
-        }
+  await cors(req, res);
+  if (req.method === "GET") {
+    try {
+      const response = await fetch(listUrl);
+      const html = await response.text();
+      const data = [];
+      const $ = cheerio.load(html);
+      $(".carousel-item a")
+        .filter((i, elem) => !$(elem).text().includes("清冊"))
+        .each((i, elem) => {
+          const titleString = $(elem).text();
+          const year = titleString.split("年")[0];
+          const re = /月中獎號碼單/gi;
+          const monthRange = titleString.split("年")[1].replace(re, "");
+          const dataLink = $(elem).attr("href");
+          data.push({
+            year,
+            monthRange,
+            dataLink,
+          });
+        });
+      res.statusCode = 200;
+      return res.json({
+        data,
+      });
+    } catch (error) {
+      return res.json({
+        data: [],
+        error: `res error`,
+      });
     }
-}
+  }
+};
